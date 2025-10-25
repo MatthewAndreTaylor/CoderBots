dc692374961e8a9eebfbb2bbc4894e23656a42d4
Matthew Taylor <matthew.andre.taylor@gmail.com>
2025-10-25 11:04:04 -0400

    progress

--- app.py
+++ app.py
@@ -1,7 +1,64 @@
 from flask import Flask, render_template
-from flask_sock import Sock
-import json
+from Box2D import b2World, b2Vec2, b2EdgeShape, b2FixtureDef, b2CircleShape
+import pygame
+
+# Server should send player physics data
+
+# when the server starts send a list 
+
 
 app = Flask(__name__)
-sock = Sock(app)
 
+WIDTH, HEIGHT = 800, 1000
+world = b2World(gravity=(0, 0), doSleep=True)
+
+player_body = world.CreateDynamicBody(
+    position=(WIDTH / 2 / 10, 5),
+    shapes=b2CircleShape(radius=2),
+    linearDamping=0.5,
+    angularDamping=0.5
+)
+
+wall_top = world.CreateStaticBody(
+    position=(0, HEIGHT / 10),
+    fixtures=b2FixtureDef(
+        shape=b2EdgeShape(vertices=[(0, 0), (WIDTH / 10, 0)]),
+        restitution=1.0
+    )
+)
+
+wall_bottom = world.CreateStaticBody(
+    position=(0, 0),
+    fixtures=b2FixtureDef(
+        shape=b2EdgeShape(vertices=[(0, 0), (WIDTH / 10, 0)]),
+        restitution=1.0
+    )
+)
+
+wall_left = world.CreateStaticBody(
+    position=(0, 0),
+    fixtures=b2FixtureDef(
+        shape=b2EdgeShape(vertices=[(0, 0), (0, HEIGHT / 10)]),
+        restitution=1.0
+    )
+)
+
+wall_right = world.CreateStaticBody(
+    position=(WIDTH / 10, 0),
+    fixtures=b2FixtureDef(
+        shape=b2EdgeShape(vertices=[(0, 0), (0, HEIGHT / 10)]),
+        restitution=1.0
+    )
+)
+
+pygame.init()
+clock = pygame.time.Clock()
+
+
+@app.route('/', methods=['GET',"POST"])
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ coderbot_package/README.md
@@ -0,0 +1,20 @@
+Here is the code for the client end points.
+
+- login
+
+- training envirments
+
+- multiplayer lobby
+
+read from sensors
+
+- lidar distance sensor
+
+- collision sensor
+
+move around robot
+
+- dx, dy
+
+- note the robot will stop on collision to avoid damage
+
